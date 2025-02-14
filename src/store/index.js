15a97406dd001b162451c8bcfc42354e6411733a
Daria Kot <poturayko@gmail.com>
2025-02-14 14:06:33 +0100

    First commit new version Serbian-words-app

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  wordReducer from "../store/slice/wordSlice";
-
-export const store = configureStore({
-    reducer: {
-        word: wordReducer,
-    },
-    devTools: process.env.NODE_ENV !== 'production',
-});
