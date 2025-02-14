33a644aa45a1c6a278ba6a85b64dc42f4eab4255
Daria Kot <poturayko@gmail.com>
2025-02-14 18:16:27 +0100

    Configured vite base

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://itgirlschool.justmakeit.ru',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: '/serbian-words-app/',
 });
