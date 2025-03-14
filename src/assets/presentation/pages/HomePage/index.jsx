87f7e8c0041664f9f5d635fd2aa2408ed60ebd2d
Daria Kot <poturayko@gmail.com>
2025-03-14 13:01:40 +0100

    minor edits

--- src/assets/presentation/pages/HomePage/index.jsx
+++ src/assets/presentation/pages/HomePage/index.jsx
@@ -1,27 +1,14 @@
 import React, { useRef } from 'react';
 import Gallery from '../../components/CardGallery';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { WordTable } from '../../components/WordTable';
-// import { AddWord } from '../../components/AddWordButton';
-// import { fetchWords } from '../../../../store/slice/wordSlice';
+
 
 function HomePage() {
-  // const dispatch = useDispatch();
-  // const { loading, error } = useSelector((state) => state.word);
 
   const focusButtonRef = useRef(null);
-  // useEffect(() => {
-  //   dispatch(fetchWords());
-  // }, [dispatch]);
 
-  // if (loading) return <h2>Loading...</h2>;
-  // if (error) return <h2>{error}</h2>;
 
   return (
     <React.Fragment>
-      {/* <AddWord/>
-      <WordTable /> */}
-
       <Gallery focusButtonRef={focusButtonRef} />
     </React.Fragment>
     );
